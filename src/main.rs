f94402f55561b54c51b42827702e7f4e282f0a4d
TheFel0x <43345523+TheFel0x@users.noreply.github.com>
2021-06-25 22:42:28 +0200

    it works!

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+fn main() {
+    println!("{}",string_to_braille("meow"));
+    println!("{}",braille_to_text("⡭⡥⡯⡷"));
+    println!("{}",braille_to_text(&string_to_braille("meow")));
+}
+
+fn byte_to_braille(by:u8) -> u16 {
+    by as u16 + 10240
+}
+
+fn braille_to_byte(br:u16) -> u8 {
+    (br - 10240) as u8
+}
+
+fn string_to_braille(s:&str) -> String {
+    let mut vec: Vec<u16> = Vec::new();
+    for c in s.chars() {
+        vec.push(byte_to_braille(c as u8));
+    }
+    String::from_utf16_lossy(&vec)
+}
+
+fn bytes_to_braille(b: Vec<u8>) -> String {
+    let mut vec: Vec<u16> = Vec::new();
+    for by in b {
+        vec.push(byte_to_braille(by as u8));
+    }
+    String::from_utf16_lossy(&vec)    
+}
+
+fn braille_to_bytes(s: &str) -> Vec<u8> {
+    let mut vec: Vec<u8> = Vec::new();
+    for c in s.chars() {
+        vec.push(braille_to_byte(c as u16));
+    }
+    vec
+}
+
+fn braille_to_text(s: &str) -> String {
+    String::from_utf8_lossy(&braille_to_bytes(s)).to_string()
+}
